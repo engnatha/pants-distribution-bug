5c453517e9c7977bbde3bb50e0a9bab74ceb91d7
engnatha <69609716+engnatha@users.noreply.github.com>
2022-10-26 10:57:55 -0600

    Distribution bug repro (#1)
    * Add some silly data files to reproduce distribution bug
    
    * Demonstrate distribution missing data files
    
    Co-authored-by: Nate England <nathanael@astranis.com>

--- BUILD
+++ BUILD
@@ -4,4 +4,23 @@
 # A macro that turns every entry in this directory's requirements.txt into a
 # `python_requirement_library` target. Refer to
 # https://www.pantsbuild.org/docs/python-third-party-dependencies.
-python_requirements(name="reqs")
+python_requirements(name = "reqs")
+
+python_distribution(
+    name = "test_distribution",
+    dependencies = ["//helloworld/greet/greeting.py:lib"],
+    entry_points = {"console_scripts": {"hello": "helloworld.greet.greating:say_hello"}},
+    provides = python_artifact(
+        name = "test_dist",
+        version = "0",
+    ),
+)
+
+pex_binary(
+    name = "executable_scripts",
+    dependencies = [
+        ":test_distribution",
+        ":reqs#conscript",
+    ],
+    script = "conscript",
+)
--- helloworld/greet/greeting.py
+++ helloworld/greet/greeting.py
@@ -3,6 +3,9 @@
 
 from __future__ import annotations
 
+from colors import green
+
+import pathlib
 import json
 import random
 
@@ -12,19 +15,30 @@ from helloworld.translator.translator import LanguageTranslator
 
 
 class Greeter:
-    def __init__(
-        self, *, translations: dict[str, dict[str, str]] | None = None
-    ) -> None:
-        self._translations = (
-            translations
-            if translations is not None
-            else json.loads(
-                pkg_resources.resource_string(__name__, "translations.json")
-            )
-        )
+    def __init__(self,
+                 *,
+                 translations: dict[str, dict[str, str]] | None = None
+                 ) -> None:
+        self._translations = (translations
+                              if translations is not None else json.loads(
+                                  pkg_resources.resource_string(
+                                      __name__, "translations.json")))
         self._translator = LanguageTranslator(self._translations)
 
     def greet(self, name: str) -> str:
         random_greeting = random.choice(list(self._translations.keys()))
-        greeting = self._translator.translate_to_random_language(random_greeting)
+        greeting = self._translator.translate_to_random_language(
+            random_greeting)
         return f"{greeting}, {name}!".capitalize()
+
+
+def say_hello() -> None:
+    greeting = Greeter().greet("Pantsbuild")
+    secret_path = pathlib.Path(
+        __file__).parent / 'helloworld/external/data/top_secret.txt'
+    print(green(greeting))
+    print(secret_path.read_text())
+
+
+if __name__ == "__main__":
+    say_hello()
